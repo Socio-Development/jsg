6369b43eb809a70aaf2633bddf892350955fc1ef
Casper Tollefsen <casper.tollefsen@telenor.no>
2024-12-11 19:57:04 +0100

    feat: add JsgNumber support to jsg and update tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { JsgBoolean } from './boolean'
+import { JsgNumber } from './number'
 import { JsgString } from './string'
 
 export default {
   boolean: () => new JsgBoolean(),
+  number: () => new JsgNumber(),
   string: () => new JsgString(),
 }
