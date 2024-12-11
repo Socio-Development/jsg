7c964a06d435f98a61527ec1ff670b807fa540e2
Casper Tollefsen <74550679+CasperSocio@users.noreply.github.com>
2024-12-11 20:01:34 +0100

    feat: add support for the JSON schema number type (#12)
    * feat: add JsgNumber class for JSON Schema number type support
    
    * test: add unit tests for JsgNumber class methods
    
    * feat: add JsgNumber support to jsg and update tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,31 @@
 import { JsgBoolean } from './boolean'
 import jsg from './index'
+import { JsgNumber } from './number'
 import { JsgString } from './string'
 
 describe('jsg', () => {
-  describe('string', () => {
-    it('returns a JsgString instance', () => {
-      const actual = jsg.string()
-      const expected = new JsgString()
+  describe('boolean', () => {
+    it('returns a JsgBoolean instance', () => {
+      const actual = jsg.boolean()
+      const expected = new JsgBoolean()
 
       expect(actual).toEqual(expected)
     })
   })
 
-  describe('boolean', () => {
-    it('returns a JsgBoolean instance', () => {
-      const actual = jsg.boolean()
-      const expected = new JsgBoolean()
+  describe('number', () => {
+    it('returns a JsgNumber instance', () => {
+      const actual = jsg.number()
+      const expected = new JsgNumber()
+
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('string', () => {
+    it('returns a JsgString instance', () => {
+      const actual = jsg.string()
+      const expected = new JsgString()
 
       expect(actual).toEqual(expected)
     })
