737546c4c4e84455c894bfa5c090c2697b9a4554
Casper Tollefsen <casper.tollefsen@telenor.no>
2024-12-12 11:01:09 +0100

    feat: add JsgFlag enum for use with JsgPrimitive

---
+++ src/enums.ts
@@ -0,0 +1,9 @@
+/**
+ * Flags that can be applied to `JsgPrimitive['_flags']`.
+ */
+export enum JsgFlag {
+  /**
+   * Indicates that the property key should not be included in `JsgObject['_props'].required`.
+   */
+  OPTIONAL = 'opt',
+}
